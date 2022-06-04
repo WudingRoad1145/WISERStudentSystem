349b8cd9a74e8d6eb410d39d9e965ab36dd39109
Feng Yan <fy48@duke.edu>
2022-06-04 20:21:16 +0300

    client update

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://wiser-student-system.herokuapp.com",
+  baseURL: "https://wiser-student-system.herokuapp.com/",
   headers: {
     "Content-type": "application/json"
   }
