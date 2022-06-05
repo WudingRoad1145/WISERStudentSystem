4e34a7585872f9b9d8eccc1dbd8822f5c57db960
Feng Yan <fy48@duke.edu>
2022-06-05 10:54:51 +0300

    new build

---
+++ client/build/precache-manifest.438840ee0aff380e330d6b8a769384ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b676f858d7cacd6ee0aa09d5ee52f2ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf0193a8fc2a0016205a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5a0f5a29ca31d9a1da81",
+    "url": "/static/css/main.b5fec8d3.chunk.css"
+  },
+  {
+    "revision": "cf0193a8fc2a0016205a",
+    "url": "/static/js/2.8091b331.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.8091b331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0f5a29ca31d9a1da81",
+    "url": "/static/js/main.0ac20d8a.chunk.js"
+  },
+  {
+    "revision": "07858fea14fd66493418",
+    "url": "/static/js/runtime-main.efee8004.js"
+  }
+]);
