f6f84ec3558f8817b0ab32b9b2ce06626d986ff3
Feng Yan <fy48@duke.edu>
2022-06-28 14:28:03 +0300

    version0.5 all parts added in frontend

--- client/src/components/student.component.js
+++ client/src/components/student.component.js
@@ -3,7 +3,7 @@ import studentDataService from "../services/student.service";
 import Select from 'react-select';
 import { MDBContainer } from "mdbreact";
 import { Bar } from "react-chartjs-2";
-import Chart from 'chart.js/auto';
+//import Chart from 'chart.js/auto';
 import "./student.css";
 import studentChart from './studentChart.js';
 
